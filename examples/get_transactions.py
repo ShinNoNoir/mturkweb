0d8432b6f87440e60019151341edaff609e86c79
Egbert Bouman <ebouman@gmail.com>
2015-02-02 14:26:15 +0100

    Added example script for downloading the transaction history

---
+++ examples/get_transactions.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import csv
+import getpass
+import StringIO
+import argparse
+
+from collections import defaultdict
+
+# TODO: fix this
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import mturkweb
+
+CONSIDER_PAYMENT_TYPES = ['AssignmentPayment', 'BonusPayment']
+
+
+def login_prompt():
+    email = raw_input('Email: ')
+    password = getpass.getpass('Password: ')
+    return email, password
+
+def parse_csv(csv_string):
+    results = defaultdict(lambda: [0,0])
+    csv_file = StringIO.StringIO(csv_string)
+    csvreader = csv.DictReader(csv_file, delimiter=',')
+    
+    for row in csvreader:
+        if row['Transaction Type'] in CONSIDER_PAYMENT_TYPES:
+            sign = 1 if row['Amount'][0] != '-' else -1
+            amount = float(row['Amount'].lstrip('-')) * sign
+            results[row['Recipient ID']][0] += amount
+            results[row['Recipient ID']][1] += 1
+            
+    csv_file.close()
+
+    sorted_results = sorted([[k, v[0], v[1]] for k, v in results.iteritems()], key=lambda x: x[1])
+    return sorted_results
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Download the transaction history from mturk')
+    
+    try:
+        parser.add_argument('year', help='The year you want the transaction history for')
+        parser.add_argument('--amount', help='Only print payments above this threshold (default: 10)', default=10)
+        
+        args = parser.parse_args(sys.argv[1:])
+        
+    except argparse.ArgumentError:
+        parser.print_help()
+        sys.exit(2)
+    
+    try:
+        email, password = login_prompt()
+        session = mturkweb.login(email, password)
+        transactions = session.get_transaction_history_csv(int(args.year))
+    except Exception, e:
+        print 'Error:', str(e)
+        sys.exit(1) 
+
+    sorted_results = parse_csv(transactions)
+    amount_threshold = args.amount * -1
+    print '\n%-20s%10s%10s\n' % ('Worker', 'Amount', '#HITS')
+    for worker, amount, times in sorted_results:
+        if amount < amount_threshold:
+            print '%-20s%10.2f%10s' % (worker, amount, times)
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
